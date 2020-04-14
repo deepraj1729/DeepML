39ca6f288e181cf3761bbf1c8774e2a56f9a56fa
Deepraj <bdeeprajrkm1@gmail.com>
2020-04-14 06:53:18 +0530

    new commit

--- layers.py
+++
@@ -1,9 +0,0 @@
-def Dense(input_shape =2 ):
-    print("\n\nDense function called")
-    print("Layers = {}".format(input_shape))
-
-def Flatten(input_shape=2):
-    pass
-
-def Input(input_shape):
-    return 9
