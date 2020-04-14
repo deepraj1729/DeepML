c847a2a1d26771fdeea3109fbf3607135bb64830
Deepraj <bdeeprajrkm1@gmail.com>
2020-04-14 06:52:36 +0530

    changed model structure

--- main.py
+++ main.py
@@ -5,30 +5,18 @@
 #   O     O   Inputs (length,width)
 
 import numpy as np
-from models import Perceptron
+import pandas as pd
+from DeepNet.models import Perceptron
 
-data = np.array([
-    [1,1.5,0],
-    [2,1,0],
-    [1.5,1.2,0],
-    [4,1.5,1],
-    [4.5,2,1],
-    [4.3,1.8,1],
-    [3,1.3,0],
-    [3.5,0.5,0],
-    [5.5,2.5,1],
-    [4.5,1.5,1],
-    [2,0.5,0],
-    [5.5,3,1],
-    [1,1,0],
-    [7.4,3.4,1]
-])
+data = pd.read_csv('Datasets/flower.csv')
 
+x = np.array(data.drop(['Sl no.','flower label'],1))
+y = np.array(data['flower label'])
 
 mystery_flower = np.array([[4.8,1.9],[2,1.5],[7,3.9]])
 
 model = Perceptron()
-model.train(data=data,epochs= 70000)
+model.train(data_X=x,data_Y=y,epochs= 50000)
 model.train__accuracy()
 model.test(mystery_flower)
 model.visualize()
