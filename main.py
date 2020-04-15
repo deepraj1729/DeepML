de8b9232a833e4533cfb29db3cdeedf3976e90c8
Deepraj <bdeeprajrkm1@gmail.com>
2020-04-15 12:08:25 +0530

    removed redundancy

--- main.py
+++ main.py
@@ -10,13 +10,12 @@ from DeepNet.models import Perceptron
 
 data = pd.read_csv('Datasets/flower.csv')
 
-x = np.array(data.drop(['Sl no.','flower label'],1))
+X = np.array(data.drop(['Sl no.','flower label'],1))
 y = np.array(data['flower label'])
 
 mystery_flower = np.array([[4.8,1.9],[2,1.5],[7,3.9]])
 
 model = Perceptron()
-model.train(data_X=x,data_Y=y,epochs= 50000)
-model.train__accuracy()
-model.test(mystery_flower)
+model.train(data_X=X,data_Y=y,epochs= 50000)
+model.pred(mystery_flower)
 model.visualize()
